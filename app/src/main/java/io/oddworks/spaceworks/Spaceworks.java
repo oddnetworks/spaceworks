0f690c2ca889bceb34c4393eb2c9ab570689ceb4
Erik Straub <erik.straub@oddnetworks.com>
2016-07-11 19:00:08 -0400

    Updates to 2.0.1-beta with one-liner

--- app/src/main/java/io/oddworks/spaceworks/Spaceworks.java
+++ app/src/main/java/io/oddworks/spaceworks/Spaceworks.java
@@ -6,25 +6,10 @@ import io.oddworks.device.request.ApiCaller;
 import io.oddworks.device.request.RestServiceProvider;
 
 public class Spaceworks extends Application {
-
-    private static Spaceworks SINGLETON;
-
-    public static Spaceworks getInstance() {
-        return SINGLETON;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
-        SINGLETON = this;
 
-        setupOddworks();
-    }
-
-    private void setupOddworks() {
-        RestServiceProvider.init(this);
-
-        ApiCaller apiCaller = RestServiceProvider.getInstance().getApiCaller();
-        apiCaller.setBaseUrl("http://odd-gdgnyc.herokuapp.com/");
+        RestServiceProvider.init(this, "http://odd-gdgnyc.herokuapp.com/");
     }
 }
